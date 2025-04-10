9e89fb1f217548adf1f947ead8099681197f85dd
tmk <tmk@exidia.com>
2025-04-11 01:28:58 +0200

    chore: remove euday astro

--- apps/site/src/env.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="../.astro/types.d.ts" />
-/// <reference types="astro/client" />
-
-declare module '*.yml' {
-  const value: any; // Add type definitions here if desired
-  export default value;
-}
