6011fd7fd0e51d45e92e1f1737a009da8f20d5bd
tmk <tmk@exidia.com>
2025-04-11 21:53:26 +0200

    chore: streamline build pipeline

--- packages/prototypes/gulpfile.js
+++ packages/prototypes/gulpfile.js
@@ -6,10 +6,9 @@ const { parallel } = gulp;
 
 export const { process: processScss, watch: watchScss } = scss;
 
-export const { process: processPrototypes, watch: watchPrototypes } =
-  prototypes;
+export const { process: buildPrototypes, watch: watchPrototypes } = prototypes;
 
-export const build = parallel(processScss, processPrototypes);
+export const build = parallel(processScss, buildPrototypes);
 
 export const watch = parallel(watchScss, watchPrototypes);
 
