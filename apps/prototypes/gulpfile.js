b54a5d7fc7d41b8916e6760107bdbb4a46a3c87c
tmk <tmk@exidia.com>
2025-04-11 02:04:06 +0200

    chore: use correct prototypes imports

--- apps/prototypes/gulpfile.js
+++ apps/prototypes/gulpfile.js
@@ -6,21 +6,13 @@ const { parallel } = gulp;
 
 export const { process: processScss, watch: watchScss } = scss;
 
-export const {
-  process: processPrototypes,
-  watch: watchPrototypes,
-  watchComponents: watchPrototypesComponents,
-} = prototypes;
+export const { process: processPrototypes, watch: watchPrototypes } =
+  prototypes;
 
 export const { transpile, watchTranspiled } = js;
 
 export const build = parallel(processScss, transpile, processPrototypes);
 
-export const watch = parallel(
-  watchScss,
-  watchTranspiled,
-  watchPrototypes,
-  watchPrototypesComponents
-);
+export const watch = parallel(watchScss, watchTranspiled, watchPrototypes);
 
 export { build as default };
