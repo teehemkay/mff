e63b1782afaf02ba86746ff565c88f690ddf202f
MABONZO Kalimasi Nzau-Mfuka <kalimasi.mabonzo@ext.europarl.europa.eu>
2025-04-11 11:20:33 +0200

    refact(scripts): don't transpile js

--- apps/prototypes/gulpfile.js
+++ apps/prototypes/gulpfile.js
@@ -1,6 +1,6 @@
 import gulp from 'gulp';
 
-import { js, prototypes, scss } from '@packages/build-tools';
+import { prototypes, scss } from '@packages/build-tools';
 
 const { parallel } = gulp;
 
@@ -9,10 +9,8 @@ export const { process: processScss, watch: watchScss } = scss;
 export const { process: processPrototypes, watch: watchPrototypes } =
   prototypes;
 
-export const { transpile, watchTranspiled } = js;
+export const build = parallel(processScss, processPrototypes);
 
-export const build = parallel(processScss, transpile, processPrototypes);
-
-export const watch = parallel(watchScss, watchTranspiled, watchPrototypes);
+export const watch = parallel(watchScss, watchPrototypes);
 
 export { build as default };
