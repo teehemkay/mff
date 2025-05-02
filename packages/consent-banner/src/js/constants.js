5b487a292257b0a0fc968d6bea8dc4839856a731
MABONZO Kalimasi Nzau-Mfuka <kalimasi.mabonzo@ext.europarl.europa.eu>
2025-05-02 15:52:04 +0200

    refact(cs): removed DNT obsolete feature

--- packages/consent-banner/src/js/constants.js
+++ packages/consent-banner/src/js/constants.js
@@ -1,6 +1,5 @@
 export const OPTED_IN = 'in';
 export const OPTED_OUT = 'out';
-export const DNTED = 'dnt';
 export const NO_CHOICE = 'choose';
 
 export const JS_AND_CSS_BASENAME = 'privacy-policy';
